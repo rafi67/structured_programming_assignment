3f7b5c966abfec9f8bf96247a526a80d8aa79229
Rafi Siddique <rafisiddique67@gmail.com>
2022-09-06 12:39:31 +0600

    bug fixed in count pattern in string

--- number_of_pattern_pos_in_string.c
+++ number_of_pattern_pos_in_string.c
@@ -1,18 +1,20 @@
 #include <stdio.h>
 
-int search(char *c1, char *c2, int size, int size2) {
-    int cnt = 0, cnt2 = 0, index = 0;
-    for(int i=0; i<size2; ++i) {
-        for(int j=0; j<size; ++j) {
-            if(c2[i]==c1[j]) {
-                ++cnt2;
-                ++i;
-            }
-            if(cnt2==size2) {
-                ++cnt;
-                cnt2 = 0;
-                i = 0;
-            }
+int counter(char *c1, char *c2, int size, int size2) {
+    int index = 0, cnt2 = 0, cnt = 0;
+    for(int i=0; i<size; ++i) {
+        if(c2[index]==c1[i]) {
+            ++cnt2;
+            if(index!=size2) ++index;
+        }
+        else if(c2[index]!=c1[i]) {
+            cnt2 = 0;
+            index = 0;
+        }
+        if(cnt2==size2) {
+            ++cnt;
+            cnt2 = 0;
+            index = 0;
         }
     }
     return cnt;
@@ -35,9 +37,9 @@ int main() {
 
     int s = size(str), s2 = size(p_str);
 
-    int pos = search(str, p_str, s, s2);
+    int p_cnt = counter(str, p_str, s, s2);
 
-    printf("%d\n", pos);
+    printf("%d\n", p_cnt);
 
     return 0;
 
